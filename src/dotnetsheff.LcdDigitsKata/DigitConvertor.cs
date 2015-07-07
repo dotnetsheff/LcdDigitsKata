2a2214a7ec73fd09d44f216e1c58dcd175dd719a
kevbite <kev_bite@msn.com>
2015-07-07 23:46:50 +0100

    Digit Converter.

---
+++ src/dotnetsheff.LcdDigitsKata/DigitConvertor.cs
@@ -0,0 +1,47 @@
+﻿namespace dotnetsheff.LcdDigitsKata
+{  
+    public class DigitConvertor
+    {
+        private static readonly string[] Digits =
+        {
+            "._.\n|.|\n|_|",
+            "...\n..|\n..|",
+            "._.\n._|\n|_."
+
+            /*
+._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+
+             */
+        };
+
+        public string Convert(int i)
+        {
+            var lines = new [] {"", "", ""};
+
+            do
+            {
+                var digit = i%10;
+
+                var digitString = ConvertSingleDigit(digit).Split('\n');
+                lines[0] = digitString[0] + " " + lines[0];
+                lines[1] = digitString[1] + " " + lines[1];
+                lines[2] = digitString[2] + " " + lines[2];
+
+                i = i/10;
+
+            } while ( i > 0);
+
+            return lines[0].Substring(0, lines[0].Length-1) + "\n" +
+                   lines[1].Substring(0, lines[1].Length-1) + "\n" + 
+                   lines[2].Substring(0, lines[2].Length-1);
+        }
+
+
+        private  string ConvertSingleDigit(int i)
+        {
+            return Digits[i];
+        }
+    }
+}
---
+++ src/dotnetsheff.LcdDigitsKata/DigitConvertorTests.cs
@@ -0,0 +1,58 @@
+﻿using Shouldly;
+
+namespace dotnetsheff.LcdDigitsKata
+{
+    public class DigitConvertorTests
+    {
+        public void GivenAnZero_ThenAnZeroDigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(0);
+
+            digit.ShouldBe("._.\n|.|\n|_|");
+        }
+
+        public void GivenAnOne_ThenAnOneDigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(1);
+
+            digit.ShouldBe("...\n..|\n..|");
+        }
+
+        public void GivenAnTwo_ThenAnTwoDigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(2);
+
+            digit.ShouldBe("._.\n._|\n|_.");
+        }
+
+        public void GivenATen_ThenAnTenDigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(10);
+
+            digit.ShouldBe("... ._.\n..| |.|\n..| |_|");
+        }
+
+        public void GivenA11_ThenAn11DigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(11);
+
+            digit.ShouldBe("... ...\n..| ..|\n..| ..|");
+        }
+
+        public void GivenA201_ThenAn201DigitIsReturned()
+        {
+            var digitConvertor = new DigitConvertor();
+            var digit = digitConvertor.Convert(201);
+
+            digit.ShouldBe( "._. ._. ...\n" +
+                            "._| |.| ..|\n" +
+                            "|_. |_| ..|");
+        }
+    }
+}
+  
